72c117c2e19543384f4208f9c822edc5e06161f8
learnerloki <loki310806@gmail.com>
2025-12-17 19:02:49 +0530

    initial-commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.className = "delete";
+
+  deleteBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    li.remove();
+  });
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+}
